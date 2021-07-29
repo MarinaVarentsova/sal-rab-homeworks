7b22f8c99ff123a3efffaf19a26c82fa88eb2823
Varentsova Marina <Varentsovamv@gmail.com>
2021-07-29 20:31:27 +0300

    не может формулу просчитать

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,16 +33,15 @@
 //    }
 // }
 
-function sendRequest(name, phone, address={street, house, entrance, floor, flat}, goods={title, count}, sum) {
-    let data = {client: {}, order: {}, goods: {}};
+function sendRequest(name, phone, address={street, house, entrance, floor, flat}, goods=[...count], sum) {
+    let data = {client: {}, order: {}, goods: []};
     data.client = name + " " + phone;
     data.order.address = "ул." + " " + address.street + ", "+ "дом" + " " + address.house + ", " + address.entrance + " подъезд" + ", " + address.floor + " этаж" + ", кв " + address.flat;
-    data.order.sum = sum;    
-    let countOfGoods = goods.length;
-    for (let i = 0; i <= countOfGoods; i ++) {
-        data.goods = goods[i];
+    data.order.sum = sum;   
+        let countOfGoods = goods.length;
+         for (let i = 0; i <= countOfGoods; i ++) {
+         data.goods.push([goods.count[i]]);
     }
-
     let jsonData = JSON.stringify({data: data});
 
     return jsonData;
